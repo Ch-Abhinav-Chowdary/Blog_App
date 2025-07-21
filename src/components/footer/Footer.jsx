2f3463affbb2d3c15ffe0b60df603c6108a799a5
ch Abhinav <chowdaryabhinav95@gmail.com>
2025-07-21 21:36:17 +0530

    Updated progress

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>Footer</div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from './Navbar.module.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className={styles.container}>Navbar</div>
   )
 }
 
