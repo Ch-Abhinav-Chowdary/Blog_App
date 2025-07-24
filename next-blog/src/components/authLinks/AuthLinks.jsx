004c89387f6f480fe9f507e5700536d1133330f5
Ch-Abhinav-Chowdary <chowdaryabhinav95@gmail.com>
2025-07-25 00:06:32 +0530

    Added a feature

--- next-blog/src/components/authLinks/AuthLinks.jsx
+++ next-blog/src/components/authLinks/AuthLinks.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+"use client"
+
+import React, { useState } from 'react';
 import styles from './AuthLinks.module.css';
 import Link from 'next/link';
 
 function AuthLinks() {
+    const [open,setOpen] = useState(false);
     //temporary
     const status = 'notauthenticated';
     return (
@@ -15,6 +18,29 @@ function AuthLinks() {
                     <span className={styles.link}>Logout</span>
                 </>
             )}
+
+            <div className={styles.burger} onClick={()=>{setOpen(!open)}}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
+            {open && 
+                <div className={styles.menu}>
+                    <Link href='/'>Homepage</Link>
+                    <Link href='/'>About</Link>
+                    <Link href='/'>Contact</Link>
+
+                    {status === 'notauthenticated' ? (
+                    <Link href="/login">Login</Link>
+                    ) : (
+                        <>
+                            <Link href="/write">Write</Link>
+                            <span className={styles.link}>Logout</span>
+                        </>
+                    )}
+                </div>
+
+            }
         </>
     )
 }
